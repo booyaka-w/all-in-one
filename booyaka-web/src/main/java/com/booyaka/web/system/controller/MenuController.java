8ad60acaaafff2d72f1a2cff04aae31fb2b167bf
Edge <Edge@Edge-PC>
2020-12-02 09:46:39 +0800

    MenuPageData

--- booyaka-web/src/main/java/com/booyaka/web/system/controller/MenuController.java
+++ booyaka-web/src/main/java/com/booyaka/web/system/controller/MenuController.java
@@ -1,11 +1,18 @@
 package com.booyaka.web.system.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.alibaba.fastjson.JSON;
+import com.booyaka.web.system.model.SysMenu;
 import com.booyaka.web.system.service.SysMenuService;
 
 /**
@@ -18,6 +25,8 @@ import com.booyaka.web.system.service.SysMenuService;
 @RequestMapping("/system/menu")
 public class MenuController {
 
+	private final String PARENT_ID = "0";
+
 	@Autowired
 	SysMenuService menuService;
 
@@ -26,4 +35,26 @@ public class MenuController {
 		return new ModelAndView("/system/menu-page");
 	}
 
+	@GetMapping("/page/data")
+	public String menuListData(SysMenu sysMenu) {
+		List<SysMenu> list = menuService.querySelective(sysMenu);
+		List<Map<String, Object>> listMap = new ArrayList<Map<String, Object>>();
+		int size = list.size();
+		for (int i = 0; i < size; i++) {
+			SysMenu temp = list.get(i);
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("id", temp.getMenuId());
+			map.put("pid", PARENT_ID.equals(temp.getParentId()) ? 0 : temp.getParentId());
+			map.put("status", temp.getMenuStatus());
+			map.put("name", temp.getMenuName());
+			map.put("icon", temp.getMenuIcon());
+			map.put("path", temp.getMenuPath());
+			map.put("subordinate", temp.getMenuSubordinate());
+			map.put("type", temp.getMenuType());
+			map.put("version", temp.getVersion());
+			listMap.add(map);
+		}
+
+		return JSON.toJSONString(listMap);
+	}
 }
