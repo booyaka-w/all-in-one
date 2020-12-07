6387e2af96e804a7fd57f616669ce5e58a2be00a
Edge <Edge@Edge-PC>
2020-12-07 16:53:23 +0800

    webflux

---
+++ booyaka-webflux/src/test/java/com/booyaka/web/BooyakaWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booyaka.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooyakaWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
