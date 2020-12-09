c73d815edf7f299851ae43446f83591974bc666b
Edge <Edge@Edge-PC>
2020-12-09 09:48:32 +0800

    ICON

--- booyaka-web/src/main/resources/static/js/system/menu-page.js
+++ booyaka-web/src/main/resources/static/js/system/menu-page.js
@@ -3,16 +3,10 @@ $(function() {
 	$table.bootstrapTable({
 		url : '/system/menu/page/data',
 		toolbar : '#toolbar',
-		showColumns : true,
-		showButtonIcons : true,
 		showButtonText : true,
 		showRefresh : true,
-		showToggle: false,
 		cache: false,
-		detailView : false,
-		detailViewIcon: false,
-		detailViewByClick: true,
-		buttonsClass:'info',
+		buttonsClass:'outline-info',
 		idField : 'id',
 		treeShowField : 'name',
 		parentIdField : 'pid',
@@ -99,18 +93,16 @@ $(function() {
 		}
 	})
 })
+
 /**
  * 搜索
- * 
- * @returns
  */
 $('#search').click(function() {
 	$table.bootstrapTable('refresh')
 })
+
 /**
  * 刷新
- * 
- * @returns
  */
 $('#refresh').click(function() {
 	$('#toolbar').find('select[name]').each(function() {
@@ -120,249 +112,10 @@ $('#refresh').click(function() {
 })
 
 /**
- * 显示模块模态框
- * 
- * @returns
+ * 时间控件
  */
-function showModal(){
-	$("#modelTitle").text("添加模块");
-	$("#modelId").val("");
-	$("#modelSubordinate").removeAttr("disabled");
-	$('#model').modal('show');
-}
-/**
- * 显示菜单模态框
- * 
- * @returns
- */
-function menuModal(parentId){
-	$("#menuTitle").text("添加菜单");
-	$("#menuId").val("");
-	$("#menuParentId").val(parentId);
-	$("#menu").modal('show');
-}
-/**
- * 显示按钮模态框
- * 
- * @returns
- */
-function buttonModal(parentId){
-	$("#buttonTitle").text("添加按钮");
-	$("#buttonId").val("");
-	$("#buttonParentId").val(parentId);
-	$('#button').modal('show');
-}
-
-/**
- * 关闭模态框
- * 
- * @param modal
- *            模态框
- * @param modalFrom
- *            表单
- * @returns
- */
-function hideModal(modal,modalFrom){
-	$(modalFrom)[0].reset();
-	$(modal).modal('hide')
-}
-
-/**
- * 保存数据
- * 
- * @param modal
- *            模态框
- * @param modalFrom
- *            表单
- * @param obj
- *            当前对象
- * @returns
- */
-function saveModal(modal,modalFrom,obj){
-	var status = $(modalFrom).parsley().validate();
-	if(status){
-		var object= {
-            url:"/menu/saveOrUpdate",
-　　 		type:"post",
-　　  		restForm:true,
-　　  		timeout:6000,
-    		beforeSend: function () {
-		        $(obj).attr({ disabled: "disabled" });
-		    },
-　　　　　　  success:function(data){
-		  		if(data.success){
-	　　　　　　  		$(modalFrom)[0].reset();
-	　　　　　　　　　　  $(modal).modal('hide');
-	　　　　　　　　　　  $table.bootstrapTable('refresh');
-		　　  		}else{
-		　　  			Messenger().post({
-		　　  				message:data.msg,
-		    				type: 'error',
-		    				showCloseButton: true
-		　　  			});
-		　　  		}
-　　  		 	},
-　　　　　　  complete: function (jqXHR) {
-				$(obj).removeAttr("disabled");
-		    },
-		}
-		$(modalFrom).ajaxSubmit(object);
-	}
-}
-
-/**
- * 修改状态
- * 
- * @param obj
- *            当前对象
- * @param id
- *            主键ID
- * @param version
- *            版本号
- * @returns
- */
-function updateMenuStatus(obj,menuId,version){
-	var status = $(obj).is(':checked')?1:2;
-	$.ajax({
-		url:"/menu/saveOrUpdate",
-		type:"post",
-		data:{menuId:menuId,menuStatus:status,version:version},
-		beforeSend: function () {
-			$(obj).attr({ disabled: "disabled" });
-	    },
-　　  	success:function(data){
-	　　  	if(data.success){
-	　　  		$table.bootstrapTable('refresh');
-	　　  	}else{
-	　　  		Messenger().post({
-	　　  			message:data.msg,
-    				type: 'error',
-    				showCloseButton: true
-	　　  		});
-		　　  }
-		},
-　　  	complete: function () {
-			$(obj).removeAttr("disabled");
-	    },
-	});
-}
-/**
- * 模块编辑
- * 
- * @param menuId
- *            主键ID
- * @returns
- */
-function editModal(menuId){
-	$.ajax({
-		url:"/menu/queryMenuById",
-		type:"get",
-		data:{menuId:menuId},
-	　　 success:function(data){
-	　　  	if(data.success){
-	　　  		var menu = data.attributes.sysMenu;
-	　　  		$('#modelId').val(menu.menuId);
-	　　  		$('#modelVersion').val(menu.version);
-	　　  		$('#modelName').val(menu.menuName);
-	    		$('#modelIcon').val(menu.menuIcon);
-	    		$("#modelSubordinate option").each(function(){
-	    			if($(this).val() == menu.menuSubordinate){
-			    	    $(this).attr("selected","selected");
-			        }
-			    });
-				$("#modelSubordinate").attr("disabled","disabled");
-	    		$("#modelStatus option").each(function(){
-			        if($(this).val() == menu.menuStatus){
-			    	    $(this).attr("selected","selected");
-			        }
-			    });
-	    		$("#modelTitle").text("编辑模块");
-	    		$('#modal').modal('show');
-	　　  	}else{
-	　　  		Messenger().post({
-	　　  			message:data.msg,
-    				type: 'error',
-    				showCloseButton: true
-	　　  		});
-	　　  	}
-		},
-	});
-}
-
-/**
- * 模块菜单
- * 
- * @param menuId
- *            主键ID
- * @returns
- */
-function editMenu(menuId){
-	$.ajax({
-		url:"/menu/queryMenuById",
-		type:"get",
-		data:{menuId:menuId},
-　　  	success:function(data){
-	　　  	if(data.success){
-	　　  		var menu = data.attributes.sysMenu;
-	　　  		$('#menuId').val(menu.menuId);
-	　　  		$('#menuVersion').val(menu.version);
-	　　  		$('#menuParentId').val(menu.parentId);
-	　　  		$('#menuName').val(menu.menuName);
-	    		$('#menuIcon').val(menu.menuIcon);
-	    		$("#menuStatus option").each(function(){
-			        if($(this).val() == menu.menuStatus){
-			    	    $(this).attr("selected","selected");
-			        }
-			    });
-	    		$('#menuPath').val(menu.menuPath);
-	    		$("#menuTitle").text("编辑菜单");
-	    		$('#menu').modal('show');
-	　　  	}else{
-	　　  		Messenger().post({
-	　　  			message:data.msg,
-    				type: 'error',
-    				showCloseButton: true
-	　　  		});
-	　　  	}
-		},
-	});
-}
-
-/**
- * 模块按钮
- * 
- * @param menuId
- *            主键ID
- * @returns
- */
-function editButton(menuId){
-	$.ajax({
-		url:"/menu/queryMenuById",
-		type:"get",
-		data:{menuId:menuId},
-　　  	success:function(data){
-	　　  	if(data.success){
-	　　  		var menu = data.attributes.sysMenu;
-	　　  		$('#buttonId').val(menu.menuId);
-	　　  		$('#buttonVersion').val(menu.version);
-	　　  		$('#buttonParentId').val(menu.parentId);
-	　　  		$('#buttonName').val(menu.menuName);
-	    		$("#buttonStatus option").each(function(){
-			        if($(this).val() == menu.menuStatus){
-			    	    $(this).attr("selected","selected");
-			        }
-			    });
-	    		$('#resource').val(menu.resource);
-	    		$('#buttonPath').val(menu.menuPath);
-	    		$("#buttonTitle").text("编辑按钮");
-	    		$('#button').modal('show');
-	　　  	}else{
-	　　  		Messenger().post({
-	　　  			message:data.msg,
-    				type: 'error',
-    				showCloseButton: true
-	　　  		});
-	　　  	}
-		},
-	});
-}
+laydate.render({
+	elem: '#time',
+	/*type: 'date',*/
+	range:'~'
+});
